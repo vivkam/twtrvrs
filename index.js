ba1793c516222ca9bd8942c5c3dede3b88937ec0
Vivian Kam <otherviv@gmail.com>
2013-12-07 14:14:21 -0800

    Ability to launch rebuild of views.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var action = process.argv.length > 2 ? process.argv[2] : null;
+var util = require('util'),
+	action = process.argv.length > 2 ? process.argv[2] : null;
 
 if (!action) {
 	console.log('usage: node index.js [backup/server]');
-} else if (action === 'db') { // temp for writing db init stuff
-	module.exports = require('./lib/couchdb.js');
+} else if (action === 'dbviews') {
+	require('./lib/couchdb.js').createOrUpdateViews(function (error, response) {
+		if (error) {
+			console.error('error creating views');
+			console.error(util.inspect(error));
+		} else {
+			console.log('view creation log: ');
+			console.log(util.inspect(response));
+		}
+	});
+
 } else if (action === 'topsy') {
-	module.exports = require('./lib/import-topsy.js');
+	require('./lib/import-topsy.js');
 } else if (action === 'backup') {
-	module.exports = require('./lib/backup.js');	
+	require('./lib/backup.js');	
 } else if (action === 'server') {
 	console.log('server not yet implemented');
 } else {
